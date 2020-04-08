109bfb7330ff25cbcda6bd09ed334617902b0eea
magbee <david.magbee@me.com>
2020-04-08 13:20:55 -0400

    extension installed and tested, console log success

--- top-tab/background.js
+++ top-tab/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: "#01abaa" }, function () {
+  chrome.storage.sync.set({color: '#01abaa'}, function () {
     console.log("This color is turquoise, just like the epic stone.");
   });
 });
